3deebedd753b6de322ff7796a7565cb5ff1b3080
nopnull <745991615@qq.com>
2024-05-10 17:58:19 +0800

    VSCode Test Init

---
+++ train1/h/main.h
@@ -0,0 +1,2 @@
+
+int swap( int *, int * );
---
+++ train1/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main()
+{
+    printf("[%s-%d]\n", __func__, __LINE__);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("hello China~%d\n", i);
+    }
+
+    int a = 10, b = 20;
+    swap(&a, &b);
+    printf("a:%d, b:%d\n", a, b);
+    return (0);
+}
+
+int swap(int * a, int * b)
+{
+    int tmp[sizeof(int) ] = {0};
+    tmp[0] = *a;
+    *a = *b;
+    *b = tmp[0];
+    return (0);
+}
